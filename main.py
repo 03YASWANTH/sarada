64e77d1c169f795ba39ac715517be73d93fc9b3f
03YASWANTH <147533275+03YASWANTH@users.noreply.github.com>
2024-01-06 08:00:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+
+
+class Mass(BoxLayout):
+    pass
+
+
+class MyApp(App):
+    pass
+
+
+MyApp().run()
